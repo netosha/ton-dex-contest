9527ecb9639a729c426b3cf38b4e5aee7b7d6175
netosha <antoha-surgut@yandex.ru>
2021-11-16 16:27:31 +0300

    Added UI elements

---
+++ src/ui/Button/Button.types.ts
@@ -0,0 +1,3 @@
+import { HTMLAttributes } from 'react';
+
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {}
---
+++ src/ui/Input/Input.types.ts
@@ -0,0 +1,3 @@
+import { HTMLAttributes } from 'react';
+
+export interface InputProps extends HTMLAttributes<HTMLInputElement> {}
